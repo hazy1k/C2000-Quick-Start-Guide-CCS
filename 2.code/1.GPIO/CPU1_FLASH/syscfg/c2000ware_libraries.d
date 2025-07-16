77296c7bbc723f83261b32cab71a6fe11a4a5b0b
hazy1k <a249007421@gmail.com>
2025-07-16 19:18:01 +0800

    code

---
+++ 2.code/1.GPIO/CPU1_FLASH/syscfg/c2000ware_libraries.d
@@ -0,0 +1,300 @@
+# FIXED
+
+syscfg/c2000ware_libraries.obj: syscfg/c2000ware_libraries.c
+syscfg/c2000ware_libraries.obj: syscfg/c2000ware_libraries.h
+syscfg/c2000ware_libraries.obj: syscfg/board.h
+syscfg/c2000ware_libraries.obj: E:/StudyNote/study_C2000/2.code/1.GPIO/device/driverlib.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_memmap.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/adc.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdbool.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_ti_config.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/linkage.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdint.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_stdint40.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/stdint.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/cdefs.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_types.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_types.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_stdint.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_stdint.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_adc.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_asysctl.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_sysctl.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_types.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cpu.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/debug.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/aes.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/interrupt.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_ints.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_pie.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_aes.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_aes_ss.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/asysctl.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cla.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_cla.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/clb.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_clb.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cmpss.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_cmpss.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cputimer.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_cputimer.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/sysctl.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_nmi.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_otp.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dac.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dac.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dcc.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dcc.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dcsm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dcsm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dma.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dma.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/ecap.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_ecap.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/epg.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_epg.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/epwm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_epwm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/eqep.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_eqep.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/erad.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_erad.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/flash.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_flash.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/fsi.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_fsi.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/gpio.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_gpio.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_xint.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/xbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_clbxbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_epwmxbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_inputxbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_outputxbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_clboutputxbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_xbar.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/hrpwm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_hrpwm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/hrpwm.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/i2c.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_i2c.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/lin.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_lin.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/mcan.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_mcanss.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_types_mcan.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/memcfg.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_memcfg.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pga.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_pga.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pin_map.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pmbus.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_pmbus.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pmbus_common.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/sci.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_sci.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/spi.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_spi.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/usb.h
+syscfg/c2000ware_libraries.obj: E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/version.h
+syscfg/c2000ware_libraries.obj: E:/StudyNote/study_C2000/2.code/1.GPIO/device/device.h
+syscfg/c2000ware_libraries.obj: E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stddef.h
+
+syscfg/c2000ware_libraries.c:
+
+syscfg/c2000ware_libraries.h:
+
+syscfg/board.h:
+
+E:/StudyNote/study_C2000/2.code/1.GPIO/device/driverlib.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_memmap.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/adc.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdbool.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_ti_config.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/linkage.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stdint.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/_stdint40.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/stdint.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/cdefs.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_types.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_types.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/machine/_stdint.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/sys/_stdint.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_adc.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_asysctl.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_sysctl.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_types.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cpu.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/debug.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/aes.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/interrupt.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_ints.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_pie.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_aes.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_aes_ss.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/asysctl.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cla.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_cla.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/clb.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_clb.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cmpss.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_cmpss.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/cputimer.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_cputimer.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/sysctl.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_nmi.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_otp.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dac.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dac.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dcc.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dcc.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dcsm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dcsm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/dma.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_dma.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/ecap.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_ecap.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/epg.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_epg.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/epwm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_epwm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/eqep.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_eqep.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/erad.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_erad.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/flash.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_flash.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/fsi.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_fsi.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/gpio.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_gpio.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_xint.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/xbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_clbxbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_epwmxbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_inputxbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_outputxbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_clboutputxbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_xbar.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/hrpwm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_hrpwm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/hrpwm.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/i2c.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_i2c.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/lin.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_lin.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/mcan.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_mcanss.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_types_mcan.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/memcfg.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_memcfg.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pga.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_pga.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pin_map.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pmbus.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_pmbus.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/pmbus_common.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/sci.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_sci.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/spi.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/inc/hw_spi.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/usb.h:
+
+E:/TI/sdk/C2000Ware_5_05_00_00/driverlib/f28p55x/driverlib/version.h:
+
+E:/StudyNote/study_C2000/2.code/1.GPIO/device/device.h:
+
+E:/TI/ccs/tools/compiler/ti-cgt-c2000_22.6.2.LTS/include/stddef.h:
+
